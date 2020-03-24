eb9e788d8203a5ea9040e5ceb82f1a454a59e43d
Oscar Andersson <oscar.andersson.200@gmail.com>
2020-03-24 16:21:44 +0100

    UI and CLI  init

--- Loopbox_CLI/Program.cs
+++ Loopbox_CLI/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Loopbox;
 
 namespace Loopbox_CLI
 {
@@ -10,6 +11,7 @@ namespace Loopbox_CLI
     {
         static void Main(string[] args)
         {
+            LoopboxLib loopbox = new LoopboxLib();
         }
     }
 }
--- Loopbox_GUI/MainWindow.xaml.cs
+++ Loopbox_GUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Loopbox;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,8 +21,10 @@ namespace Loopbox_GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        LoopboxLib loopbox;
         public MainWindow()
         {
+            loopbox = new LoopboxLib();
             InitializeComponent();
         }
     }
