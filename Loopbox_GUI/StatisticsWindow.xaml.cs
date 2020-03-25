6d77d255be5c616db4234101f761109804f3dbc2
Oscar Andersson <oscar.andersson.200@gmail.com>
2020-03-25 01:48:14 +0100

    Statistics window

---
+++ Loopbox_GUI/StatisticsWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Loopbox_GUI
+{
+    /// <summary>
+    /// Interaction logic for StatisticsWindow.xaml
+    /// </summary>
+    public partial class StatisticsWindow : Window
+    {
+        public StatisticsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e) => Close();
+
+        private void btnMissing_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnQuality_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnNoPlaylist_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
