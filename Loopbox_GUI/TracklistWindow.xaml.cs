5fe65af2873cd281faed1795d7ad141bf48d53ba
Oscar Andersson <oscar.andersson.200@gmail.com>
2020-03-25 19:14:09 +0100

    adds track view and tracklist view

---
+++ Loopbox_GUI/TracklistWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static Loopbox.Config;
+
+namespace Loopbox_GUI
+{
+    /// <summary>
+    /// Interaction logic for TracklistWindow.xaml
+    /// </summary>
+    public partial class TracklistWindow : Window
+    {
+        List<Track> tracks;
+        public TracklistWindow(List<Track> tracks)
+        {
+            this.tracks = tracks;
+            InitializeComponent();
+        }
+    }
+}
