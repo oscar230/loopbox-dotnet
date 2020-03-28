5e44351503308945f99e4574d2fa0ef80e2cc4b1
Oscar Andersson <oscar.andersson.200@gmail.com>
2020-03-28 12:22:00 +0100

    Update tests and add test enviorment files

---
+++ test/LoopboxTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Loopbox;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace test
+{
+    [TestClass]
+    public class LoopboxTest
+    {
+        [TestMethod]
+        public void Only4Tracks()
+        {
+            LoopboxLib lbl = new LoopboxLib();
+            lbl.Load(SetupXML.Get4Tracks);
+            Assert.IsTrue(lbl.IsLoaded());
+            Assert.AreEqual(lbl.GetTracksCount(), 4);
+            Assert.AreEqual(lbl.GetAllPlaylistsCount(), 0);
+            Assert.AreEqual(lbl.GetTracksExistsCount(), 0);
+        }
+        [TestMethod]
+        public void Large()
+        {
+            LoopboxLib lbl = new LoopboxLib();
+            lbl.Load(SetupXML.GetLarge);
+            Assert.IsTrue(lbl.IsLoaded());
+        }
+        [TestMethod]
+        public void Playlists2()
+        {
+            LoopboxLib lbl = new LoopboxLib();
+            lbl.Load(SetupXML.Get2Playlists);
+            Assert.IsTrue(lbl.IsLoaded());
+            Assert.AreEqual(lbl.GetAllPlaylistsCount(), 2);
+        }
+    }
+}
---
+++ test/SetupXML.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    static class SetupXML
+    {
+        private const string _dir = @"rekordboxtest";
+        public static string Get4Tracks => Path.Combine(_dir, @"4tracks.xml");
+        public static string GetLarge => Path.Combine(_dir, @"large.xml");
+        public static string Get2Playlists => Path.Combine(_dir, @"2playlists.xml");
+    }
+}
